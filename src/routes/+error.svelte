1e3122276239f21c144e8f9ad52c35eaeb56fe80
charmanita <hdroberson23@gmail.com>
2026-05-07 01:46:43 -0500

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div>
+	{#if $page.status === 404}
+		<h1 class="text-white">404 page not found...</h1>
+		<img src="/images/stare.png" alt="error" />
+	{:else}
+		<h1 class="text-white">{$page.status}</h1>
+		<p>{$page.error?.message}</p>
+		<img src="/images/stare.png" alt="error" />
+	{/if}
+	<a href="/">charmanita.dev</a>
+</div>
