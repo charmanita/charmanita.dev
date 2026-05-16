e9b7224d1dd24f3ef54550d6ddff1b0389d58325
charmanita <hdroberson23@gmail.com>
2026-05-16 02:59:46 -0500

    all the blog files moved to separate repository!

--- src/posts/hello-world.md
+++
@@ -1,7 +0,0 @@
----
-title: Hello... is this thing on?
-date: 2026-04-24
-description: Just testing things out!
----
-
-Just set base in blog.charmanita.dev!
--- src/routes/blog/[slug]/+page.ts
+++
@@ -1,7 +0,0 @@
-export async function load({ params }) {
-  const post = await import(`../../../posts/${params.slug}.md`);
-  return {
-    content: post.default,
-    meta: post.metadata
-  };
-}
