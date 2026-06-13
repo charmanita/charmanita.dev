e4479698bea0df48a0a321bc39df58471266df8a
charmanita <hdroberson23@gmail.com>
2026-06-13 00:30:29 -0500

    switching from mcapi subdomain to api subdomain

--- src/routes/api/bots/+server.ts
+++ src/routes/api/bots/+server.ts
@@ -2,7 +2,7 @@ import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
-    const res = await fetch('https://mcapi.charmanita.dev/public/bots');
+    const res = await fetch('https://api.charmanita.dev/public/bots');
     const data = await res.json();
     return json(data);
 };
--- src/routes/api/commits/+server.ts
+++ src/routes/api/commits/+server.ts
@@ -2,7 +2,7 @@ import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
-    const res = await fetch('https://mcapi.charmanita.dev/public/commits');
+    const res = await fetch('https://api.charmanita.dev/public/commits');
     const data = await res.json();
     return json(data);
 };
