a7edcbe8e0f79ef4fd0f99cbd7470329106f86d5
charmanita <hdroberson23@gmail.com>
2026-07-24 20:06:45 -0500

    removed api subroute

--- src/routes/api/status/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async () => {
-    const res = await fetch('https://api.charmanita.dev/public/status');
-    const data = await res.json();
-    return json(data);
-}
