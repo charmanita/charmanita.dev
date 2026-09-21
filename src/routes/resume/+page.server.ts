763da961304093a61f141028606a6da2ccb6cb1b
charmanita <hdroberson23@gmail.com>
2026-09-20 23:23:19 -0500

    changed link on resume sublink

--- src/routes/resume/+page.server.ts
+++ src/routes/resume/+page.server.ts
@@ -3,5 +3,5 @@ import type {PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
     // 301 - perm redirect. 
-    throw redirect(307, 'https://docs.google.com/document/d/1LtMuD6hg-Bl1NxpAoifkqTf3rVmJXUqC/edit?usp=sharing&ouid=110547424443856240539&rtpof=true&sd=true');
+    throw redirect(307, 'https://docs.google.com/document/d/1mIccKkDNMkIBKJ3-hCBmDUcYda-kS-Bq/edit?usp=sharing&ouid=110547424443856240539&rtpof=true&sd=true');
 }
